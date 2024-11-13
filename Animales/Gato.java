bfed9ac56a1628cadff3825dccc12fd1a8d9a10d
Ortiz Agustin Emmanuel <165100989+ortizagustine@users.noreply.github.com>
2024-11-12 21:22:33 -0300

    Create Gato

---
+++ Animales/Gato.java
@@ -0,0 +1,13 @@
+package javaejemplo.animales;
+
+public class Gato extends Animal {
+
+    public Gato() {
+        super();
+    }
+
+    @Override
+    public void sonido() {
+        System.out.println("miau nya");
+    }
+}
